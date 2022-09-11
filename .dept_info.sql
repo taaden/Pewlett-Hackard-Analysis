b8dba47f5f12e11224a54a39f1f2cb9f50d95da9
Temitope Adeniyi <tadeniyi@css.edu>
2022-09-10 22:32:36 -0400

    adding dept_info

---
+++ .dept_info.sql
@@ -0,0 +1,10 @@
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
