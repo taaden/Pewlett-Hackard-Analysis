77fe90a6dbf482e8eeafc36804bb1efd09621136
Temitope Adeniyi <tadeniyi@css.edu>
2022-09-11 19:25:05 -0400

    adding sales_dept_info.sql, sales_development_dept_info.sql

---
+++ sales_development_dept_info.sql
@@ -0,0 +1,10 @@
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name 
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN  ('Sales','Development');
